ad2dd16e5df17163220833ee324b9a3ff0cf2970
Max Afonov <max@bumnetworks.com>
2013-11-11 01:25:27 -0500

    teh first commit!!!11!!

---
+++ salvo-cli.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class SalvoCli < Formula
+  homepage 'https://github.com/maxaf/salvo'
+  url 'http://deb.bumnetworks.com.s3.amazonaws.com/pool/main/s/salvo-cli/salvo-cli_0.2.2_all.deb'
+  version '0.2.2'
+  sha1 'fdc1dfc98034ecc149f823dbd6b3c7963bd96211'
+
+  depends_on 'pbzip2'
+
+  def install
+  end
+end
