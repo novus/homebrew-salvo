7b416fc6665d6fa457f430372058bce7975a6f3e
Max Afonov <max@bumnetworks.com>
2013-11-11 12:42:14 -0500

    unpack .deb using "ar" instead of some random locally installed "tar"

--- salvo-cli.rb
+++ salvo-cli.rb
@@ -2,13 +2,14 @@ require 'formula'
 
 class SalvoCli < Formula
   homepage 'https://github.com/maxaf/salvo'
-  url 'http://deb.bumnetworks.com.s3.amazonaws.com/pool/main/s/salvo-cli/salvo-cli_0.2.2_all.deb'
+  url 'http://deb.bumnetworks.com.s3.amazonaws.com/pool/main/s/salvo-cli/salvo-cli_0.2.2_all.deb', :using => :nounzip
   version '0.2.2'
   sha1 'fdc1dfc98034ecc149f823dbd6b3c7963bd96211'
 
   depends_on 'pbzip2'
 
   def install
+    system 'ar -x salvo-cli_0.2.2_all.deb'
     system 'tar zxf data.tar.gz'
     inreplace 'usr/bin/salvo', /^SALVO_JAR_DIR=.+/, "SALVO_JAR_DIR=#{share}"
     bin.install 'usr/bin/salvo' => 'salvo'
